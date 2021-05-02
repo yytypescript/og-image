c9449d6d0f60c0c46a5735d1891fe87dbf971a43
d_etteiu8383 <ysdr83@gmail.com>
2021-05-03 01:53:33 +0900

    KaTeXの追加

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,16 +1,11 @@
 
 import { readFileSync } from 'fs';
 import marked from 'marked';
-import TexRenderer from 'marked-tex-renderer';
 import { sanitizeHtml } from './sanitizer';
 import { ParsedRequest } from './types';
 const twemoji = require('twemoji');
 const twOptions = { folder: 'svg', ext: '.svg' };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
-
-marked.setOptions({
-    renderer: new TexRenderer()
-});
 
 const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
 const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
@@ -170,12 +165,25 @@ export function getHtml(parsedReq: ParsedRequest) {
     const { text, pattern, md, fontSize, overlay } = parsedReq;
     return `<!DOCTYPE html>
 <html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous">
+        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js" integrity="sha384-g7c+Jr9ZivxKLnZTDUhnkOnsh30B4H0rpLUpJ4jAIKs4fnJI+sEnkvrMWph2EDg4" crossorigin="anonymous"></script>
+        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/contrib/auto-render.min.js" integrity="sha384-mll67QQFJfxn0IYznZYonOWZ644AWYC+Pt2cHqMaRhXVrursRwvLnLaebdGIlYNa" crossorigin="anonymous"></script>
+    </head>
     <meta charset="utf-8">
     <title>Generated Image</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
-        ${getCss(fontSize, (overlay||''))}
+        ${getCss(fontSize, (overlay || ''))}
     </style>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+        renderMathInElement(document.body, {delimiters: [
+            {left: "\\[", right: "\\]", display: true},
+            {left: "$", right: "$", display: false}
+            ]});
+        });
+    </script>
     <body class="${pattern}">
         <div>
             <div class="overlay">
